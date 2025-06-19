da6584948f27497a0bd407db32651efa54391d69
Takatochi <nazera55tigr66t28@gmail.com>
2025-06-19 12:58:04 +0300

    feat: add Cat model structure

--- internal/models/cat.go
+++ internal/models/cat.go
@@ -1,1 +1,9 @@
 package models
+
+type Cat struct {
+	ID         uint    `json:"id"`
+	Name       string  `json:"name"`
+	Experience int     `json:"experience"`
+	Breed      string  `json:"breed"`
+	Salary     float64 `json:"salary"`
+}
